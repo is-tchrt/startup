18e9a1fae1ebf07d8af5bf15b731d45fdaa7273f
Isaac Teichert <isaac@debian.byu.edu>
2025-02-12 18:22:55 -0700

    Add jsx file for each component

---
+++ src/item/item.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Item() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>item displayed here</div>
+    </main>
+  );
+}

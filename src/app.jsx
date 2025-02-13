65c0b0139306714139964d76d419eed321e7cd14
Isaac Teichert <isaac@debian.byu.edu>
2025-02-12 18:18:46 -0700

    Add shared html to App component

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return <div className="body bg-dark text-light">
+    <header>
+      <nav className="navbar navbar-light navbar-expand-lg">
+        <a href="#"><img src="checklist.png" height="50" align="left"/></a>
+        <ul className="navbar-nav">
+          <li className="nav-item"><a href="index.html" className="nav-link">Login</a></li>
+          <li className="nav-item"><a href="group.html" className="nav-link">Group Selection</a></li>
+          <li className="nav-item"><a href="list.html" className="nav-link">List</a></li>
+          <li className="nav-item"><a href="item.html" className="nav-link">Edit Items</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>App components go here</main>
+    <footer>
+      <span>Isaac Teichert</span>
+      <a href="https://github.com/is-tchrt/startup">Github</a>
+    </footer>
+    </div>;
 }

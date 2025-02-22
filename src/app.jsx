74c3bccf29f31d7b8b2eb6075e85326d24b6b3c1
Isaac Teichert <isaac@debian.byu.edu>
2025-02-21 19:28:20 -0700

    Restrict unauthenticated access to private pages

--- src/app.jsx
+++ src/app.jsx
@@ -8,6 +8,7 @@ import { List } from './list/list';
 import { Item } from './item/item';
 
 export default function App() {
+  const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
   const [loggedIn, setLoggedIn] = React.useState(false);
   const [group, setGroup] = React.useState('');
   
@@ -18,7 +19,7 @@ export default function App() {
           <nav className="navbar navbar-light navbar-expand-lg">
             <NavLink to=""><img src="checklist.png" height="50" align="left"/></NavLink>
             <ul className="navbar-nav">
-              <li className="nav-item"><NavLink to="" className="nav-link">Login</NavLink></li>
+              {!loggedIn && (<li className="nav-item"><NavLink to="" className="nav-link">Login</NavLink></li>)}
               {loggedIn && (<li className="nav-item"><NavLink to="group" className="nav-link">Group Selection</NavLink></li>)}
               {loggedIn && group && (<li className="nav-item"><NavLink to="list" className="nav-link">List</NavLink></li>)}
               {loggedIn && group && (<li className="nav-item"><NavLink to="item" className="nav-link">Edit Items</NavLink></li>)}
@@ -27,10 +28,10 @@ export default function App() {
         </header>
 
         <Routes>
-          <Route path='/' element={<Login />} exact />
-          <Route path='/group' element={<Group />} />
-          <Route path='/list' element={<List />} />
-          <Route path='/item' element={<Item />} />
+          <Route path='/' element={<Login userName={userName} onLogin={(userName) => {setUserName(userName)}} />} exact />
+          <Route path='/group' element={loggedIn ? <Group /> : <NotAllowed />} />
+          <Route path='/list' element={loggedIn ? <List /> : <NotAllowed />} />
+          <Route path='/item' element={loggedIn ? <Item /> : <NotAllowed />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
 
@@ -46,4 +47,8 @@ export default function App() {
 
 function NotFound() {
   return <main className="container-fluid bg-secondary text-center">404: Not Found</main>;
+}
+
+function NotAllowed() {
+  return <main className="container-fluid bg-secondary text-center">Please login to access this page.</main>;
 }

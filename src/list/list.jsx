db1f1413f27d1d42a4a5f3623dd98950679a058b
Isaac Teichert <isaac@debian.byu.edu>
2025-02-22 21:50:04 -0700

    Update list page to update based on list in memory

--- src/list/list.jsx
+++ src/list/list.jsx
@@ -1,24 +1,48 @@
 import React from 'react';
+import { ListGroupItem } from 'react-bootstrap';
 import './list.css';
 
 export function List() {
+  const [list, setList] = React.useState(localStorage.getItem('list') || []);
+
+  function formatList(todoList) {
+    let listItems = [];
+    for (item of todoList) {
+      listItems.push(
+        <div className="todo">
+          <input type="checkbox" />
+          <label>{item.title}</label>
+          <span>-- Added by {item.author}</span><br/>
+        </div>
+      );
+    }
+    return listItems;
+  }
+  
   return (
     <main className="list">
       <div>
         <h2>To-do:</h2>
         <form name="todo">
-          <div className="todo">
-            <input type="checkbox" />
-            <label>Finish HTML</label>
-            <span>-- Added by Isaac</span><br></br>
-          </div>
-          <div className="todo">
-            <input type="checkbox" />
-            <label>Add CSS</label>
-            <span>-- Added by Isaac</span><br></br>
-          </div>
+          {list.map((item) => (
+            <div className="todo">
+              <input type="checkbox" />
+              <label>{item.title}</label>
+              <span>-- Added by {item.author}</span><br/>
+            </div>
+          ))}
         </form>
       </div>
     </main>
   );
 }
+          // <div className="todo">
+          //   <input type="checkbox" />
+          //   <label>Finish HTML</label>
+          //   <span>-- Added by Isaac</span><br></br>
+          // </div>
+          // <div className="todo">
+          //   <input type="checkbox" />
+          //   <label>Add CSS</label>
+          //   <span>-- Added by Isaac</span><br></br>
+          // </div>

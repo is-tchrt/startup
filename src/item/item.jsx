b8be1cb51198d1e47497a10622fbbd2d1de04610
Isaac Teichert <isaac@debian.byu.edu>
2025-02-27 11:56:30 -0700

    Add persistent item edits

--- src/item/item.jsx
+++ src/item/item.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { listItem } from '../list/listItem';
 import './item.css';
 
 export function Item(props) {
-  const item = props.itemData.item;
-  const selectedItemIndex = props.itemData.index;
+  // const item = props.itemData.item;
+  const selectedItemData = JSON.parse(localStorage.getItem('currentItem'));
+  // const selectedItemIndex = props.itemData.index;
+  const selectedItemIndex = selectedItemData.index;
+  const author = selectedItemData.item.author;
   const [title, setTitle] = React.useState(props.itemData.item.title);
   const [description, setDescription] = React.useState(props.itemData.item.description);
   const navigate = useNavigate();
-  console.log(props);
+  console.log(selectedItemData);
 
   function submitEdits(item) {
-    props.setList(props.list.map((element, index) => index === selectedItemIndex ? item : element));
-    props.setCurrentItem({item: item, index: selectedItemIndex});
-    localStorage.setItem('list', JSON.stringify(props.list));
+    // props.setList(props.list.map((element, index) => index === selectedItemIndex ? item : element));
+    // props.setCurrentItem({item: item, index: selectedItemIndex});
+    const list = JSON.parse(localStorage.getItem('list'));
+    list[selectedItemIndex] = item;
+    localStorage.setItem('list', JSON.stringify(list));
     localStorage.setItem('currentItem', JSON.stringify({item: item, index: selectedItemIndex}));
     navigate('/list');
   }
@@ -26,7 +32,7 @@ export function Item(props) {
         <div className="form-group">
           <textarea value={description} onChange={(e) => setDescription(e.target.value)} className="form-control" placeholder="Lorem ipsum" cols="40" rows="6"></textarea>
         </div>
-        <button type="submit" className="btn btn-primary" onClick={() => submitEdits({title: title, description: description, author: props.userName})} disabled={!title}>Submit</button>
+        <button type="submit" className="btn btn-primary" onClick={() => submitEdits(new listItem(title, description, author))} disabled={!title}>Submit</button>
       </form>
     </main>
   );

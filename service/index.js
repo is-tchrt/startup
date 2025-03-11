d57eae05bf4b2ffe46e119e3c4cbf70614d87c61
Isaac Teichert <isaac@debian.byu.edu>
2025-03-11 16:04:12 -0600

    Add server configuration

--- service/index.js
+++ service/index.js
@@ -1,3 +1,24 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const uuid = require('uuid');
+const app = express();
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
+const authCookieName = 'token';
+
+// Store users and groups in memory. The todo list
+// for each group is stored within that group in the
+// groups list.
+let users = [];
+let groups = [];
+
+app.use(express.json());
+
+app.use(cookieParser());
+
 app.use(express.static('public'));
+
+var apiRouter = express.Router();
+app.use('/api', apiRouter);

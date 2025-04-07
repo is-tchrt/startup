56f6b357e19a308e210df4617e87e15a55cd5aa7
Isaac Teichert <isaac@debian.byu.edu>
2025-04-07 17:16:23 -0600

    Remove dead code, console logs and commented code from front end

--- service/webSocket.js
+++ service/webSocket.js
@@ -6,20 +6,13 @@ function webSocket(httpServer) {
 
   socketServer.on('connection', (socket, req) => {
     socket.isAlive = true;
-    console.log("connected on websocket");
-    // group = DB.getUserByToken(req.cookies['token']).then((user) => user.group);
-    // socket.group = group;
 
     socket.on('message', async (data) => {
-      console.log("connected on websocket, message");
       if (data.toString() === 'listUpdate') {
-        console.log("Processing a list update");
-        // list = await DB.getGroup(socket.group).then((group) => group.list);
         group = await DB.getGroup(socket.group);
         list = group.list;
         socketServer.clients.forEach((client) => {
           if (client !== socket) {
-            console.log("sending a message to clients");
             client.send("updateList");
           }
         });
@@ -29,8 +22,6 @@ function webSocket(httpServer) {
     });
 
     socket.on('pong', () => {
-    console.log("connected on websocket: pong");
-      console.log(socketServer.clients.size);
       socket.isAlive = true;
     });
   });
